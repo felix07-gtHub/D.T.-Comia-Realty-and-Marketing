a4ac50a24d1d636d43eaf254a4d9eea5bc7db518
felix07-gtHub <olan.johnfelix@gmail.com>
2025-11-05 10:20:14 +0800

    made some changes

--- server.js
+++ server.js
@@ -1734,11 +1734,19 @@ app.get('/user-information', (req, res) => {
             
         connection.query(selectUserReservationQuery, userId, (err, selectUserReservationResult) => {
           if (err) {throw err};
-            
-            res.json({userInformation: selectUserInformationResult,
-                      userImage: selectUserImageResult,
-                      userReservation: selectUserReservationResult
-                    });
+
+          const emailAddress = selectUserInformationResult[0].email_address;
+          const recoveryEmailAddress = selectUserInformationResult[0].recovery_email_address;
+
+            //  SELECT EMAIL ADDRESS QUERY.
+          const selectEmailAddressQuery = 'SELECT type_of_email_address, old_email_address, new_email_address, token, date_expired, attempt_count, date_attempted FROM email_address_table WHERE (old_email_address = ? || old_email_address = ?)';
+          
+            //  DECLARES selectEmailAddressValue.
+          const selectEmailAddressValue = [emailAddress, recoveryEmailAddress];
+
+          connection.query(selectEmailAddressQuery, selectEmailAddressValue, (err, selectEmailAddressResult) => {
+            if(err) {throw err};
+          });
         });
       });
     });
